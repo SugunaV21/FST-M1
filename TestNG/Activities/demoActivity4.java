4cab624673621e47c72fe1230f705c84b2aa7d92
SugunaV21 <142587072+SugunaV21@users.noreply.github.com>
2023-09-04 11:32:02 +0530

    Add files via upload

---
+++ TestNG/Activities/demoActivity4.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import org.testng.annotations.Test;
+
+public class demoActivity4 {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from demoActivity4 Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from demoActivity4 Class");
+    }
+}
